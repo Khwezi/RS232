2cc3adcd69aa97c80479a30290dc06341e28dd1d
Khwezi Mngoma <khwezi@msn.com>
2023-10-11 22:36:51 +0200

    Implemented basic and passive serial port listener

---
+++ RS232.Models/Configuration/SerialOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace RS232.Models.Configuration;
+
+public class SerialOptions
+{
+    public string PortName { get; set; }
+
+    public int BaudRate { get; set; }
+
+    public TimeSpan ReadTimeout { get; set; }
+
+    public TimeSpan WriteTimeout { get; set; }
+}
---
+++ RS232.SerialRider/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using RS232.Models.Configuration;
+using System.IO.Ports;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+var settings = configuration.GetRequiredSection(nameof(SerialOptions)).Get<SerialOptions>();
+
+ArgumentNullException.ThrowIfNull(settings);
+
+ArgumentException.ThrowIfNullOrEmpty(settings.PortName, nameof(settings.PortName));
+
+if (settings.BaudRate <= 0) 
+    throw new ArgumentException("BaudRate is invalid");
+
+var serialPort = new SerialPort
+{
+    BaudRate = settings.BaudRate,
+    PortName = settings.PortName,
+    Parity = Parity.None,
+    DataBits = 8,
+    StopBits = StopBits.One,
+    Handshake = Handshake.None,
+    WriteTimeout = (int)settings.WriteTimeout.TotalSeconds,
+    ReadTimeout = (int)settings.ReadTimeout.TotalSeconds,
+};
+
+var cancellationTokenSource = new CancellationTokenSource();
+var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+serialPort.Open();
+
+Console.WriteLine($"Port IsOpen: {serialPort.IsOpen}");
+
+if (serialPort.IsOpen)
+    serialPort.WriteLine("AT");
+
+do
+{
+    var line = serialPort.ReadLine();
+
+    if(!string.IsNullOrEmpty(line.Trim()))
+        Console.WriteLine(line);
+} 
+while (await periodicTimer.WaitForNextTickAsync(cancellationTokenSource.Token) && !cancellationTokenSource.IsCancellationRequested && serialPort.IsOpen);
+
+serialPort.Close();
+
+Console.WriteLine("Serial port closed, shutting down");
+
+await periodicTimer.WaitForNextTickAsync();
+periodicTimer.Dispose();
+
+cancellationTokenSource.Cancel();
+await Task.Delay(TimeSpan.FromSeconds(5));
